f28a2e76559be48265b4e085e25ba71d43193966
tomburger64 <tom64bonde@gmail.com>
2025-05-09 15:52:43 +0200

    initial code

--- learning-useRef/src/App.tsx
+++ learning-useRef/src/App.tsx
@@ -1,7 +1,13 @@
+import { useState } from "react"
+
 export default function App() {
+  const [name, setName] = useState('')
+
   return (
     <>
-      
+      {/* state gets updated to the input value */}
+      <input value={name} onChange={e => setName(e.target.value)} />
+      <div>My name's {name}</div>
     </>
   )
 }
