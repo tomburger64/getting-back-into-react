d705ce9d9241c17519520d42672d8bedfaa3f2b7
tomburger64 <tom64bonde@gmail.com>
2025-05-15 15:39:49 +0200

    setup sumup project w/ tailwind (+react & ts)

---
+++ SUMUP/src/components/Welcome.tsx
@@ -0,0 +1,8 @@
+export default function Welcome() {
+    return (
+        <div className="flex justify-center bg-sky-500 w-96">
+            <h1>Welcome !</h1>
+            <h3>Here, you'll find a simple sum-up of different concepts I learned in React</h3>
+        </div>
+    )
+}
