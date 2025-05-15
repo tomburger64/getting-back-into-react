84edc8705a46e3ed7f7b0fc0181be768e4caf82f
tomburger64 <tom64bonde@gmail.com>
2025-05-15 16:48:18 +0200

    page container done & getting & getting tailwind docs

--- SUMUP/src/App.tsx
+++ SUMUP/src/App.tsx
@@ -1,9 +1,31 @@
 import './App.css'
+import Welcome from './components/Welcome'
 
 export default function App() {
   return (
     <>
-      
+      {/* page bg */}
+      <div className='bg-gray-400 w-100% p-5 flex justify-center'>
+
+        {/* page container */}
+        <div className='bg-yellow-300 container'>
+
+          {/* components */}
+          <Welcome />
+
+          <br />
+          <br />
+          <br />
+
+          <Welcome />
+          <hr />
+          <Welcome />
+  
+          <div><Welcome /></div>
+
+        </div>
+
+      </div>
     </>
   )
 }
---
+++ SUMUP/src/components/Welcome.tsx
@@ -0,0 +1,5 @@
+export default function Welcome() {
+    return(
+        <h1 className="m-5 text-5xl text-center bg-red-500">Welcome !</h1>
+    )
+}
