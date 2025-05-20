d7b5c5b4879b29677b15b74ee570bbffc2714441
tomburger64 <tom64bonde@gmail.com>
2025-05-20 13:33:46 +0200

    some fixes, image size problems (tring to fix with tailwind only)

--- SUMUP/src/components/Components.tsx
+++ SUMUP/src/components/Components.tsx
@@ -8,13 +8,26 @@ export default function Components(props: Props) {
     return (
         <>
             <h3 className="text-white text-3xl m-5">{props.title}</h3>
-            <img className="w-md" src={props.img} alt="" />
+            <img className="w-md max-w-10vw" src={props.img} alt="" />
 
             <p className="text-white m-2 text-lg">Starting with the basics: Components.</p> <br />
 
-            <p>Components are a simple way of grouping / dividing HTML into several parts using Javascript.</p> <br /><br />
-            
-            <p>They allow you to have your code seperated across different files, for instance, a file for a header, another for a section, etc.</p>
+            <p className="text-white m-2 text-lg">Components are a simple way of grouping / dividing HTML into several parts using Javascript. <br />
+            They allow your code to be seperated in different files, for instance, a file for a header, another for a section, etc.</p>
+
+            <p className="text-white m-2 text-lg">If you didn't use components, with Javascript only you’d need to call each html tag in JS using something like:</p>
+            <pre className="text-white m-2 text-lg whitespace-pre-wrap bg-gray-700 min-w-100">
+                <code>
+{`document.getElementById("header-container").innerHTML = \`
+    <header>
+        <h1>Welcome</h1>
+        <button>Click me</button>
+    </header>
+\`;`}
+                </code>
+            </pre>
+
+            <p className="text-white m-2 text-lg">You probably already did something like that if you learned some Javascipt before, and it's what React does with components !</p>
         </>
     )
 }
--- SUMUP/src/components/Welcome.tsx
+++ SUMUP/src/components/Welcome.tsx
@@ -1,3 +1,5 @@
+import reactLogo from '../assets/react.svg'
+
 export default function Welcome() {
     return(
         <>
@@ -5,6 +7,7 @@ export default function Welcome() {
             <h3 className="text-white m-2 text-center text-xl">Here you'll find individual components explanations that I learned so far !</h3>
 
             <p className="text-white m-2 text-center text-xl">This is a way for me to solidify what I know, and also show it with examples, and it'll also motivate me to ad more concepts here</p>
+            <div className='flex justify-center'><img className='w-20 mt-5' src={reactLogo} alt="" /></div>
         </>
     )
 }
